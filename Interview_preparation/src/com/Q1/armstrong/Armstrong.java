c28d6c5d41c71e83b64254b2601d2ba120bd5c6d
CodeByRubenInamdar <rubeninamdar@gmail.com>
2024-10-04 09:26:29 +0530

    Commit

--- Interview_preparation/src/com/Q1/armstrong/Armstrong.java
+++ Interview_preparation/src/com/Q1/armstrong/Armstrong.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Armstrong {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter The Number: ");    //153 (1)3 + (5)3 + (3)3 = 1+125+27  = 153
+		System.out.println("Enter The Number: "); // 153 (1)3 + (5)3 + (3)3 = 1+125+27 = 153
 		int Number = scanner.nextInt();
 
 		int b = Number, c = 0;
-		while (b > 0) {                               //using a while loop
+		while (b > 0) { // using a while loop
 			int d = b % 10;
 			c += d * d * d;
 			b = b / 10;
 		}
 		if (c == Number) {
-			System.out.println("Armstrong");           //use conditional statement checking the number
+			System.out.println("Armstrong"); // use conditional statement checking the number
 		} else {
 			System.out.println("Not Armstrong");
 		}
--- Interview_preparation/src/com/Q2/palindrome/Palindrome.java
+++ Interview_preparation/src/com/Q2/palindrome/Palindrome.java
@@ -3,20 +3,19 @@ package com.Q2.palindrome;
 import java.util.Scanner;
 
 public class Palindrome {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Create a Scanner object for user input
-        System.out.println("Enter The Number: ");		
-        int Number = scanner.nextInt(); // Take the input from user
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); // Create a Scanner object for user input
+		System.out.println("Enter The Number: ");
+		int Number = scanner.nextInt(); // Take the input from user
 
-        int b = 0, c = Number;         
-        while (Number != 0) {             // Loop until Number becomes 0
-            int d = Number % 10;             // Get the last digit of the Number
-            b = b * 10 + d;                // Build the reversed number
-            Number /= 10;                 // Remove the last digit from Number
-        }
+		int b = 0, c = Number;
+		while (Number != 0) { // Loop until Number becomes 0
+			int d = Number % 10; // Get the last digit of the Number
+			b = b * 10 + d; // Build the reversed number
+			Number /= 10; // Remove the last digit from Number
+		}
 
-        
-        System.out.println(c == b ? "Palindrome" : "Not Palindrome"); // Output the result
-        scanner.close(); // Close the scanner
-    }
+		System.out.println(c == b ? "Palindrome" : "Not Palindrome"); // Output the result
+		scanner.close(); // Close the scanner
+	}
 }
